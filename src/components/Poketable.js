6a5b7a42b58921fc1055550c5ad3791335846053
Ramon Victor Cardoso <ramonvc19@gmail.com>
2023-05-12 13:44:07 -0300

    Added responsiveness to Pokecards

--- src/components/Poketable.js
+++ src/components/Poketable.js
@@ -1,29 +1,16 @@
-import styles from "./Poketable.module.css"
+import styles from "./Poketable.module.css";
 import Pokecard from "./Pokecard";
 
-function Poketable({page}){
-
-    return <>
-  
-          <table className={styles.poketable}> 
-              {[...Array(6)].map((_, index) => (
-                <Pokecard num={(page*18 - 18) + index + 1} key={index} page={page}/>
-              ))}
-          </table>
-
-          <table className={styles.poketable}>
-              {[...Array(6)].map((_, index) => (
-                <Pokecard num={(page*18 - 18) + index + 7} key={index + 7} page={page}/>
-              ))}
-          </table>
-          
-          <table className={styles.poketable}>
-              {[...Array(6)].map((_, index) => (
-                <Pokecard num={(page*18 - 18) + index + 13} key={index + 13} page={page}/>
-              ))}
-          </table>
-          
-          </>
-
+function Poketable({ page }) {
+	return (
+		<>
+			<div className={styles.poketable}>
+				{[...Array(18)].map((_, index) => (
+					<Pokecard num={page * 18 - 18 + index + 1} key={index} page={page} />
+				))}
+			</div>
+		</>
+	);
 }
-export default Poketable
+
+export default Poketable;
